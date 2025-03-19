ba63aa9ef9d2e1d0524d60bfc79041ce96c4c2c2
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-19 17:30:43 -0500

    feat: wrap App components with UseCardContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import UseCard from '@context/UseCard'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(    
   <StrictMode>
-    <App />
+    <UseCard>
+      <App />
+    </UseCard>
   </StrictMode>,
 )
